91e0ff4cb008bfa801061db6ede29413eeda0dae
Ti Bone <ti-bone@koresuite.org>
2024-02-15 12:38:29 +0100

    Remove User and AddedBy as gorm doesn't fully support complex relationships(and they are redundant here).

--- src/models/Note.go
+++ src/models/Note.go
@@ -10,8 +10,6 @@ import "gorm.io/gorm"
 type Note struct {
 	gorm.Model
 	UserID    int64
-	User      User `gorm:"foreignKey:UserID"`
 	AddedByID int64
-	AddedBy   User `gorm:"foreignKey:UserID"`
 	Text      string
 }
