ee5184dd81e4d8048be890db830817382670e8cb
Ti Bone <ti-bone@koresuite.org>
2024-02-21 13:31:30 +0100

    Fix 'fatal error: concurrent map writes' and 'fatal error: bad map state' on concurrent rates.Check() call.

--- src/rates/Limiter.go
+++ src/rates/Limiter.go
@@ -5,10 +5,16 @@
 
 package rates
 
-import "time"
+import (
+	"sync"
+	"time"
+)
 
 // chats - is a map of chat ids to the last time of request, they have made
 var chats = make(map[int64]int64)
+
+// chatsLock - mutex for chats map
+var chatsLock sync.Mutex
 
 // Check - checks if the request has made in the last N(controlled by delay parameter) seconds in this chat,
 // if so, returns false, else true
@@ -16,6 +22,9 @@ var chats = make(map[int64]int64)
 // chatId - id of the chat
 // delay - delay in seconds
 func Check(chatId int64, delay int64) bool {
+	chatsLock.Lock()
+	defer chatsLock.Unlock()
+
 	lastRequest, exists := chats[chatId]
 
 	if exists && (lastRequest+delay > time.Now().Unix()) {
